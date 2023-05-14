a2d9006e362e37ae522b06cb4a4a85be33c48981
Victoria Axentii <peta-holding@yandex.ru>
2023-05-14 11:49:46 +0300

    запуск gunicorn

--- src/api/run.sh
+++ src/api/run.sh
@@ -2,5 +2,6 @@
 
 set -e
 
-# Ниже команды для запуска API
-# По идее старт gunicorn
+# Start server
+echo "Starting server"
+gunicorn --worker-class=uvicorn.workers.UvicornWorker --workers=4 -b ${PROJECT_HOST}:${PROJECT_PORT} src.app:app
